0ee1e343fd6ffe9b6e87a6573e0d02f0701578db
ngocduy <ngocduy250604@gmail.com>
2026-04-19 01:22:15 +0700

    Duy's commit

--- Backend/app/src/main/java/cms/app/AppApplication.java
+++ Backend/app/src/main/java/cms/app/AppApplication.java
@@ -2,12 +2,20 @@ package cms.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppApplication.class, args);
+  }
 
 }
