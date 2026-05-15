4c936153761a335c0919908e6d1bdc04a6f6e146
hieundt3048 <hieundt3048@ut.edu.vn>
2026-05-15 17:33:09 +0700

    API thanh toán viện phí

---
+++ Backend/app/src/main/java/cms/app/Service/IInvoiceService.java
@@ -0,0 +1,20 @@
+package cms.app.Service;
+
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import cms.app.Dto.CreateInvoiceRequest;
+import cms.app.Dto.InvoiceResponse;
+import cms.app.Dto.PayInvoiceRequest;
+
+public interface IInvoiceService {
+
+    InvoiceResponse createInvoice(CreateInvoiceRequest request);
+
+    List<InvoiceResponse> listMyInvoices(String patientUserEmail);
+
+    InvoiceResponse getInvoice(String userEmail, Iterable<? extends GrantedAuthority> authorities, Integer invoiceId);
+
+    InvoiceResponse payInvoice(String patientUserEmail, Integer invoiceId, PayInvoiceRequest request);
+}
