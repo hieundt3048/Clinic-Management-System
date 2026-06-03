bb5470e0cf79301c0cec7b21cad2dee065c70d2b
hieundt3048 <hieundt3048@ut.edu.vn>
2026-06-03 13:56:10 +0700

    done on main

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,25 +1,14 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import MedicalHistory from './pages/MedicalHistory';
 import './App.css';
-
-function Home() {
-  return (
-    <div className="text-3xl font-bold underline">
-      Hello World!
-      <nav className="flex gap-4">
-        <Link to="/register">Go to Register Page</Link>
-        <Link to="/login">Go to Login Page</Link>
-      </nav>
-    </div>
-  );
-}
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<MedicalHistory />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
